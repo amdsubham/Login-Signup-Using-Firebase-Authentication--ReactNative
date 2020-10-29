d80b38cae520f1cf0e5437d9490f2176905f2356
amdsubham <sub.subham9574@gmail.com>
2020-10-29 15:42:09 +0530

    Onboarding UI

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import OnboardingUI from './screens/OnboardingUI.js';
 import Login from './screens/Login.js';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Stack = createStackNavigator();
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator headerMode='none'>
-        <Stack.Screen name="Home" component={OnboardingUI} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
-
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+    console.log('value'+value)
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      }
+      else {
+        setIsFirstLaunch(false);
+      }
+    })
+  })
+  console.log('isFirstLaunch'+isFirstLaunch)
+  if (isFirstLaunch == null) {
+    return null;
+  }
+  else if (isFirstLaunch == true) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator headerMode='none'>
+          <Stack.Screen name="Home" component={OnboardingUI} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+  else {
+    return <Login />
+  }
 };
 
 
